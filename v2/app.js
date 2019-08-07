e716b04e752d7a2831f1df3c015872be52b35d9b
yuanwentian <yuanwentian97@gmail.com>
2019-08-07 15:48:38 +0800

    add v2

---
+++ v2/app.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Granite Hill",
+// 		image: "https://vq5m02e0f663b3ski25j8sfq-wpengine.netdna-ssl.com/wp-content/uploads/2019/04/KimCarroll.com_KimCarroll.com_EVERGREEN_0014.jpg"
+// 	}, function(err, campground){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("NEWLY CREATED CAMPGROUND: ");
+// 			console.log(campground);
+// 		}
+// 	});
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+	//Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});
+});
+
+//where you create a campground
+app.post("/campgrounds", function(req, res){
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	//Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+//show the form where you can add a campground
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+})
+
+app.listen(port, process.env.IP, function(){
+	console.log("The YelpCamp Server Has Started");
+});
